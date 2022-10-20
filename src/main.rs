332d2b9fdff502a6faba229596f6993169268e14
Federico Biondi <fdbion@gmail.com>
2022-10-20 00:12:53 -0300

    feat: setup api entry point

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+mod api;
+
+use api::task::{
+    get_task
+}
+
+use actix_web::{HttpServer, App, web::Data, middleware::Logger};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "debug");
+    std::env::set_var("RUST_BACKTRACE", "1");
+    env_logger::init();
+
+    HttpServer::new(move || {
+        App::new()
+        .wrap(Logger::default())
+        .service(get_task);
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await;
 }
