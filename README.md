17a7aa9d7b6fce864609f1488424a6756b431e39
Federico Biondi <fdbion@gmail.com>
2022-10-22 22:10:22 -0300

    feat: actix web api

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ## Test API using Rust with Actix framework!
 
 [Actix Docs](https://actix.rs/docs/)
+[Rust API Sample](https://github.com/Me163/youtube)
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 mod api;
+mod model;
+mod repository;
 
 use api::task::{
-    get_task
-}
+    get_task,
+    submit_task,
+    stast_task,
+    complete_task,
+    fail_task,
+    pause_task
+};
+
+use repository::ddb::DDBRepository;
 
 use actix_web::{HttpServer, App, web::Data, middleware::Logger};
 
@@ -12,12 +21,28 @@ async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_BACKTRACE", "1");
     env_logger::init();
 
+    let config = aws_config::load_from_env().await;
     HttpServer::new(move || {
+
+        let ddb_repo: DDBRepository = DDBRepository::init(
+            String::from("task"),
+            config.clone()
+        );
+        let ddb_data = Data::new(
+            ddb_repo
+        );
+        let logger = Logger::default();
         App::new()
-        .wrap(Logger::default())
-        .service(get_task);
+            .wrap(logger)
+            .app_data(ddb_data)
+            .service(get_task)
+            .service(submit_task)
+            .service(stast_task)
+            .service(complete_task)
+            .service(fail_task)
+            .service(pause_task)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("127.0.0.1", 80))?
     .run()
-    .await;
+    .await
 }
