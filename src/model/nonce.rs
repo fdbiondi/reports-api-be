d2de5904d15d05ebf726d78b7411751d67804480
Federico Biondi <fdbion@gmail.com>
2022-11-19 18:45:12 -0300

    feat: add nonce

--- src/model/nonce.rs
+++ src/model/nonce.rs
@@ -1,21 +1,61 @@
+use core::result::Result;
 use serde::Serialize;
+use sqlite::{Connection, Error as sqERR};
 use uuid::Uuid;
 
 #[derive(Serialize)]
-pub struct Report {
-	pub uuid: String,
-	pub signature: String,
-	pub nonce: i32
+pub struct Nonce {
+    pub uuid: String,
+    pub signature: String,
+    pub nonce: i32,
+}
+
+#[derive(Debug)]
+pub enum NonceErr {
+    DbErr(sqERR),
+}
+
+impl From<sqERR> for NonceErr {
+    fn from(s: sqERR) -> Self {
+        NonceErr::DbErr(s)
+    }
+}
+
+fn open_connection() -> Result<Connection, sqERR> {
+    let conn = sqlite::open("../data/mydb.sqlite")?;
+
+    Ok(conn)
 }
 
 // signature NVARCHAR(132) PRIMARY KEY NOT NULL
 // nonce INTEGER NOT NULL
 impl Nonce {
-	pub fn new(signature) -> Report {
-		Nonce {
-			uuid: Uuid::new_v4().to_string(),
-			signature,
-			nonce: 1
-		}
-	}
+    // get nonce -> search by signature
+
+    pub fn add(signature: String) -> Result<String, NonceErr> {
+        let conn = open_connection()?;
+        let mut db =
+            conn.prepare("INSERT INTO nonces (uuid, signature, nonce) VALUES (?, ?, ?);")?;
+
+        let nonce = Nonce::new(signature);
+
+        db.bind(1, nonce.uuid.as_bytes())?;
+        db.bind(2, nonce.signature.as_bytes())?;
+        db.bind(3, nonce.nonce.to_string().as_bytes())?;
+        db.next()?;
+
+        Ok(nonce.uuid)
+    }
+
+    pub fn find() -> Nonce {
+        Nonce::new(String::from("test"))
+    }
+
+    pub fn new(signature: String) -> Nonce {
+        Nonce {
+            uuid: Uuid::new_v4().to_string(),
+            signature,
+            nonce: 1,
+        }
+    }
 }
--- src/model/report.rs
+++ src/model/report.rs
@@ -1,12 +1,12 @@
 use serde::Serialize;
+use strum_macros::{Display, EnumString};
 use uuid::Uuid;
-use strum_macros::{EnumString, Display};
 
 #[derive(Serialize, EnumString, Display, Eq, PartialEq)]
 pub enum ReportState {
     InProgress,
     Completed,
-    Failed
+    Failed,
 }
 
 #[derive(Serialize)]
@@ -15,21 +15,20 @@ pub struct Report {
     pub signature: String,
     pub description: String,
     pub title: String,
-    pub state: ReportState
+    pub state: ReportState,
 }
 
 // signature NVARCHAR(132) PRIMARY KEY NOT NULL
 // description TEXT NOT NULL
 // title NVARCHAR(50) NOT NULL)
-
 impl Report {
     pub fn new(signature: String, title: String, description: String) -> Report {
         Report {
-			uuid: Uuid::new_v4().to_string(),
-			signature,
-			description,
+            uuid: Uuid::new_v4().to_string(),
+            signature,
+            description,
             title,
-			state: ReportState.InProgress
+            state: ReportState::InProgress,
         }
     }
 }
